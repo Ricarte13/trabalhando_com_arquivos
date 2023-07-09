3e6dea2dacbda922d623eaec89322d4cbf2e60ce
Vinícius Ricarte <viniciusdiv@hotmail.com>
2023-07-09 10:58:06 -0300

    Writing a new file

--- src/application/Exercise.java
+++ src/application/Exercise.java
@@ -1,29 +1,70 @@
 package application;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+
+import entities.Product;
 
 public class Exercise {
 
 	public static void main(String[] args) {
 		
-		String path = "F:\\tmp\\itens.cvs.txt";
+		Locale.setDefault(Locale.US);
+		Scanner scanner = new Scanner(System.in);
 		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+		List<Product> list = new ArrayList<>();
+		
+		System.out.print("Enter a file path: ");
+		String sourceFileStr = scanner.nextLine();
+		
+		File sourceFile = new File(sourceFileStr);
+		String sourceFolderStr = sourceFile.getParent();
+		
+		boolean success = new File(sourceFolderStr + "\\out").mkdir();
+		
+		String targetFileStr = sourceFolderStr + "\\out\\summary.cvs.txt";
+		
+		try (BufferedReader br = new BufferedReader(new FileReader(sourceFileStr))) {
 			
-			String line = br.readLine();
+			String itemCvs = br.readLine();
 			
-			while(line != null) {
-				System.out.println(line);
-				line = br.readLine();
+			while(itemCvs != null) {
+				String[] fields = itemCvs.split(",");
+				String name = fields[0];
+				double price = Double.parseDouble(fields[1]);
+				int quantity = Integer.parseInt(fields[2]);
+				
+				list.add(new Product(name, price, quantity));
+				
+				itemCvs = br.readLine();
 			}
 			
-		}
+			try(BufferedWriter bw = new BufferedWriter(new FileWriter(targetFileStr))){
+				for(Product item : list) {
+					bw.write(item.getProductName() + ", " + String.format("%.2f", item.total()));
+					bw.newLine();
+				}
+				
+				System.out.println(targetFileStr + "CREATED!");
+			}
+			
 		catch (IOException e){
-			System.out.println("Error: " + e.getMessage());
+			System.out.println("Error writing file: " + e.getMessage());
 		}
-
+	}
+	catch (IOException e) {
+		System.out.println("Error reading file: " + e.getMessage());
+	}
+		
+	scanner.close();
 	}
 
 }
